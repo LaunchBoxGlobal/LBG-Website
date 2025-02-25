f69f0dc844f6854ffdb0d60b71c7b6b938f651c7
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-02-25 13:40:31 +0500

    changed contact us content

--- components/ContactUs/ContactUsHero.jsx
+++ components/ContactUs/ContactUsHero.jsx
@@ -8,9 +8,9 @@ const ContactUsHero = () => {
       <h1 className="font-bold text-4xl lg:text-[64px] mb-1 text-center tracking-normal lg:leading-[78px]">
         Contact <span className="red-text">Us</span>
       </h1>
-      <p className="section-paragraph text-center">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br />{" "}
-        Cupiditate, reprehenderit!
+      <p className="text-base lg:text-[24px] text-center 2xl:text-[30px] font-light lg:leading-[36px] text-gray-400 lg:w-2/3">
+        Have questions or ideas? Let’s connect and create <br /> something great
+        together!
       </p>
     </section>
   );
