c39f6e85019d21a2ad5e58605e49fddf9e5923f5
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-02-28 09:43:41 +0500

    fixed service accordion alignment

--- components/Global/ServiceCard.jsx
+++ components/Global/ServiceCard.jsx
@@ -21,7 +21,7 @@ const ServiceCard = ({ service, index, openFaq, toggleFaq }) => {
         <span className="text-[20px] font-medium text-gray-400 group-hover:text-white transition-all duration-300">{`(0${
           index + 1
         })`}</span>
-        <div className="flex items-start justify-between w-[80%] lg:w-[750px] 2xl:lg:w-[70%] border">
+        <div className="flex items-start justify-between w-[80%] lg:w-[750px] 2xl:lg:w-[70%]">
           <h3
             className={`text-lg text-start md:text-[34px] lg:text-[40px] xl:text-[45px] 2xl:text-[55px] leading-[45px] tracking-tight font-light ${
               openFaq === index ? "text-white" : "text-gray-400"
