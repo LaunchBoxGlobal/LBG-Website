c89f2080d867143cdbeea24a51ef601a6db0dfdc
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-03-05 10:03:55 +0500

    changed puzzle image

--- components/Home/HomeComparison.jsx
+++ components/Home/HomeComparison.jsx
@@ -5,40 +5,40 @@ const HomeComparison = () => {
   return (
     <section className="w-full padding-x py-12 lg:py-28 bg-[#F9F9F9]">
       <section className="w-full grid grid-cols-1 lg:grid-cols-2 gap-10">
-        <section className="flex flex-col items-center lg:items-start text-center lg:text-start">
-          <h2 className="text-2xl lg:text-[35px] xl:text-[45px] 2xl:text-[60px] font-bold mb-6">
+        <section className="flex flex-col items-center lg:items-center justify-start text-center lg:px-5">
+          <h2 className="text-2xl lg:text-[35px] xl:text-[40px] 2xl:text-[60px] font-bold mb-6">
             <strong>
               Problems You <span className="text-[#F40E00]">Face</span>
             </strong>
           </h2>
-          <p className="text-xl xl:text-[22px] font-light text-gray-400 mb-5 lg:mb-0">
-            Lack of transparency and poor methods lead <br /> to glitches,
-            delays, and ultimate failures.
+          <p className="text-xl xl:text-[18px] font-light leading-6 text-gray-400 mb-5 lg:mb-0">
+            Poor communication and outdated methods <br /> cause delays,
+            glitches, and project failures.
           </p>
           <Image
             src={"/problems-image.png"}
             width={400}
             height={386}
             alt="problems-image"
-            className="xl:mt-9 w-[85%] xl:h-[465px] mx-auto lg:ml-0"
+            className="xl:mt-20 w-[80%] mx-auto"
           />
         </section>
-        <section className="flex flex-col items-center lg:items-start text-center lg:text-start">
-          <h2 className="text-2xl lg:text-[35px] xl:text-[45px] 2xl:text-[60px] font-bold mb-6">
+        <section className="flex flex-col items-center lg:items-center text-center">
+          <h2 className="text-2xl lg:text-[35px] xl:text-[40px] 2xl:text-[60px] font-bold mb-6">
             <strong>
               Solutions We<span className="text-[#F40E00]"> Provide</span>
             </strong>
           </h2>
-          <p className="text-xl xl:text-[22px] font-light text-gray-400 mb-5 lg:mb-3">
-            You will be informed at every step, using Agile <br /> methods to
-            keep your project on track and up to date.
+          <p className="text-xl xl:text-[18px] font-light  leading-6 text-gray-400 mb-5 lg:mb-5">
+            Stay informed at every step with Agile methods, <br /> ensuring a
+            smooth and on-track project.
           </p>
           <Image
-            src={"/solutions-we-provide-puzzle.png"}
+            src={"/puzzle-animation-gray.png"}
             width={509}
             height={500}
             alt="solutions we provide"
-            className="w-[88%] opacity-90"
+            className="w-[80%] mx-auto opacity-90"
           />
         </section>
       </section>
