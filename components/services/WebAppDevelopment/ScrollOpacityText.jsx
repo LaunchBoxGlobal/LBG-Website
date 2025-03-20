b5e02dd853af53921274cdc692943b024d2a2a6b
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-03-20 11:57:22 +0500

    fixing about section heading alignment

--- components/services/WebAppDevelopment/ScrollOpacityText.jsx
+++ components/services/WebAppDevelopment/ScrollOpacityText.jsx
@@ -85,7 +85,7 @@ export function TextMaskEffect() {
     <section className={"web-app-main"}>
       <h2
         className={
-          "absolute left-1/2 -translate-x-1/2 text-[#f40e00] text-[25px] font-semibold top-2 lg:top-[6%] xl:top-[8%] w-full text-center"
+          "absolute left-1/2 -translate-x-1/2 text-[#f40e00] text-[25px] font-semibold top-2 lg:top-2%] xl:top-[4%] w-full text-center"
         }
       >
         Your Trusted Web App Development Services Partner
