8639f896317c0b9ff4375da873269e3ba3633b92
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-07-03 15:22:20 +0500

    created animation on hiring hassels section on staff augmentation page

--- components/StaffAugmentation/HiringHasselsAnimation.jsx
+++ components/StaffAugmentation/HiringHasselsAnimation.jsx
@@ -6,7 +6,7 @@ import { useInView } from "react-intersection-observer";
 
 const HiringHasselsAnimation = () => {
   const { ref, inView } = useInView({
-    triggerOnce: true,
+    triggerOnce: false,
   });
   return (
     <div className="w-full relative overflow-hidden">
@@ -20,25 +20,48 @@ const HiringHasselsAnimation = () => {
 
       <div
         ref={ref}
-        className="flex h-[230px] w-[230px] absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10  items-center justify-center"
+        className="flex h-[430px] w-[430px] absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10  items-center justify-center"
       >
-        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-300/80 opacity-45"></span>
-        <span className="animate-ping absolute inline-flex h-[80%] w-[80%] rounded-full bg-gray-300/80 opacity-60"></span>
-        <span className="animate-ping absolute inline-flex h-[60%] w-[60%] rounded-full bg-gray-300/80 opacity-75"></span>
-        <Image
-          src={"/staff-augmentation/exclamation-mark-image.png"}
-          alt="exclamation-mark-image"
-          width={266}
-          height={266}
-          className="absolute inline-flex z-20"
-        />
+        <motion.div
+          initial={{ scale: 0.3 }}
+          animate={inView ? { scale: 1 } : { scale: 0.3 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="border border-gray-300 absolute inline-flex h-full w-full rounded-full bg-gray-100 opacity-50"
+        ></motion.div>
+        <motion.div
+          initial={{ scale: 0.3 }}
+          animate={inView ? { scale: 1 } : { scale: 0.3 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="border border-gray-300 absolute inline-flex h-[80%] w-[80%] rounded-full bg-gray-100 opacity-60"
+        ></motion.div>
+        <motion.div
+          initial={{ scale: 0.3 }}
+          animate={inView ? { scale: 1 } : { scale: 0.3 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="border border-gray-300 absolute inline-flex h-[60%] w-[60%] rounded-full bg-gray-100 opacity-75"
+        ></motion.div>
+
+        <motion.div
+          initial={{ scale: 0.3 }}
+          animate={inView ? { scale: 1 } : { scale: 0.3 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="absolute inline-flex  z-20"
+        >
+          <Image
+            src={"/staff-augmentation/exclamation-mark-image.png"}
+            alt="exclamation-mark-image"
+            width={266}
+            height={266}
+            className="z-20"
+          />
+        </motion.div>
         {/* <span className="relative inline-flex rounded-full h-[100px] w-[100px] bg-sky-500"></span> */}
         {/* icons */}
         <motion.div
           initial={{ scale: 0.3 }}
           animate={inView ? { scale: 1 } : { scale: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute left-[30%] top-[-45%]"
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          className="absolute left-[30%] top-[-5%]"
         >
           <Image
             src={"/staff-augmentation/hiring-hassels-icon-1.png"}
@@ -51,8 +74,8 @@ const HiringHasselsAnimation = () => {
         <motion.div
           initial={{ scale: 0.3 }}
           animate={inView ? { scale: 1 } : { scale: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute -left-10 top-[-20%]"
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          className="absolute left-[10%] top-[15%]"
         >
           <Image
             src={"/staff-augmentation/hiring-hassels-icon-2.png"}
@@ -65,22 +88,22 @@ const HiringHasselsAnimation = () => {
         <motion.div
           initial={{ scale: 0.3 }}
           animate={inView ? { scale: 1 } : { scale: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute top-[-10%] right-[-10%] z-10"
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          className="absolute top-[25%] right-[10%] z-10"
         >
           <Image
             src={"/staff-augmentation/hiring-hassels-icon-3.png"}
             alt="hiring-hassels-icon-3"
-            width={121}
-            height={133}
+            width={131}
+            height={143}
             className="object-contain z-10"
           />
         </motion.div>
         <motion.div
           initial={{ scale: 0.3 }}
           animate={inView ? { scale: 1 } : { scale: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute bottom-[5%] right-[-30%] z-10"
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          className="absolute bottom-[20%] right-[5%] z-10"
         >
           <Image
             src={"/staff-augmentation/hiring-hassels-icon-4.png"}
@@ -93,8 +116,8 @@ const HiringHasselsAnimation = () => {
         <motion.div
           initial={{ scale: 0.3 }}
           animate={inView ? { scale: 1 } : { scale: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute bottom-[-40%] left-1/2 -translate-x-1/2 z-10"
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          className="absolute bottom-[-4%] left-1/2 -translate-x-1/2 z-10"
         >
           <Image
             src={"/staff-augmentation/hiring-hassels-icon-5.png"}
@@ -107,8 +130,8 @@ const HiringHasselsAnimation = () => {
         <motion.div
           initial={{ scale: 0.3 }}
           animate={inView ? { scale: 1 } : { scale: 0.3 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className="absolute bottom-[10%] left-[-20%] z-10"
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          className="absolute bottom-[22%] left-[12%] z-10"
         >
           <Image
             src={"/staff-augmentation/hiring-hassels-icon-6.png"}
