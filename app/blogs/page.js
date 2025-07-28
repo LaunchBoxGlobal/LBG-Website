71641ceb11eec5d9d0f2010ef689bb6e4b6fba57
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-07-28 10:19:04 +0500

    removed sitemap and robot.txt files

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,14 +1,14 @@
 import BlogsPage from "@/components/Blogs/BlogsPage";
 import React from "react";
 
-// export const metadata = {
-//   title: "Explore Our Blog Archive | Latest Stories & Creative Ideas",
-//   description:
-//     "Browse our blog archive for practical advice, inspiring stories, and tips to grow your skills. Fresh content updated weekly!",
-//   alternates: {
-//     canonical: "https://launchboxglobal.com/blogs",
-//   },
-// };
+export const metadata = {
+  title: "Explore Our Blog Archive | Latest Stories & Creative Ideas",
+  description:
+    "Browse our blog archive for practical advice, inspiring stories, and tips to grow your skills. Fresh content updated weekly!",
+  alternates: {
+    canonical: "https://launchboxglobal.com/blogs",
+  },
+};
 
 const page = () => {
   return <BlogsPage />;
